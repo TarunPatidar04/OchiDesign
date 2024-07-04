9f0f28ba83550f80dcd6cd6037ecd984bb49b045
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-07-04 12:23:21 +0530

    create a basic configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="w-full h-screen bg-zinc-900">Hello world!</div>
+    </div>
+  );
+};
+
+export default App;
