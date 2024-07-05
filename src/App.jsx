3ed1e9148cf942caa667bd11863753093d2bb0c6
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-07-05 11:14:40 +0530

    create a featured component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import About from "./components/About";
 import Eyes from "./components/Eyes";
+import Features from "./components/Features";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Marquee/>
       <About/>
       <Eyes/>
+      <Features/>
       </div>
   );
 };
